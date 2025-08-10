066d27f7d60ce9982ce5d11d7e2e25b36dd7d8d7
reuterShawn <reutershawn45@gmail.com>
2025-08-10 17:23:22 -0500

    Adjusted Dialouge box and replaced ColorRect with panel.

--- dialogue_system_01.gd
+++ dialogue_system_01.gd
@@ -1,45 +1,41 @@
 # DialogueSystem.gd
-extends Control
+extends Node2D
 
+@onready var dialogue_text = $DialogueText
+@onready var character_portrait = $CharacterPortrait
+@onready var character_name_label = $CharacterName
 
 var dialogue_output = []
 var current_dialogue_index = 0
-var current_character = "John"
-
+var current_character = ""
 
 # Signal for when dialogue ends
 signal dialogue_finished
 
-#import resource guy.png
-var testtexture = load("res://Assets/guy.png") #TODO: fallback
-
 func _ready():
-	# Make Function that appends all strings from JSON file
-	# Or use JSON file in future for dialogue
-	dialogue_output.append("Hello let us start")
-	dialogue_output.append("You are creating a river, where water will run through.")
-	dialogue_output.append("Start by clicking a node three squares to the right"
-)
-	dialogue_output.append("You cannot go through the tree, so you will have to go around."
-)
-
-	start_dialogue(current_character, testtexture, dialogue_output)
-	$ContinueButton.pressed.connect(_on_continue_pressed)
+	start_dialogue(dialogue_text, character_portrait, character_name_label)
+	$DialogueBox.pressed.connect(_on_continue_pressed)
 	#hide_dialogue()
 
-	
 # Method 1: Using individual parameters (your current approach)
 func start_dialogue(character_name: String, portrait_texture: Texture2D, dialogue_lines: Array):
 	current_character = character_name
 	dialogue_output = dialogue_lines
 	current_dialogue_index = 0
 	
-	$DialogueBox/CharacterName.text = character_name  # Fixed the bug here
-	$DialogueBox/CharacterPortrait.texture = portrait_texture
+	character_name_label.text = character_name  # Fixed the bug here
+	character_portrait.texture = portrait_texture
 	
 	show_dialogue()
 	display_current_line()
 
+# Method 2: Using DialogueData resource
+func start_dialogue_from_resource(dialogue_data: DialogueData):
+	start_dialogue(
+		dialogue_data.character_name,
+		dialogue_data.character_portrait,
+		dialogue_data.dialogue_lines
+	)
 
 # Method 3: Using dictionary (good for JSON or simple data)
 func start_dialogue_from_dict(dialogue_dict: Dictionary):
@@ -51,7 +47,7 @@ func start_dialogue_from_dict(dialogue_dict: Dictionary):
 
 func display_current_line():
 	if current_dialogue_index < dialogue_output.size():
-		$DialogueBox/DialogueText.text = dialogue_output[current_dialogue_index]
+		dialogue_text.text = dialogue_output[current_dialogue_index]
 
 func _on_continue_pressed():
 	current_dialogue_index += 1
@@ -62,10 +58,10 @@ func _on_continue_pressed():
 
 func show_dialogue():
 	visible = true
-#
+
 func hide_dialogue():
 	visible = false
-#
+
 func end_dialogue():
 	hide_dialogue()
-#	dialogue_finished.emit()  # Emit signal when dialogue ends
+	dialogue_finished.emit()  # Emit signal when dialogue ends
