66f8094ff9856515989831e3faa638e18eec4294
dallen72 <dallen72@outlook.com>
2025-08-13 07:50:54 -0400

    added another level; changed the size of the tree and cow to smaller

--- Levels/level_3.gd
+++ Levels/level_3.gd
@@ -2,7 +2,7 @@ extends 'res://Levels/level.gd'
 
 # need to call super
 func _ready():
-	PlayerEnergy.player_energy = 400
-	Global.energy_changed.emit(400)
+	PlayerEnergy.player_energy = 600
+	Global.energy_changed.emit(600)
 	super()
 	$UI.init_hud()
